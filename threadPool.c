10ead81be417f230589b1d54d768208c0ace6826
LizAharonian <lizaharonian@gmail.com>
2018-05-28 00:01:00 +0300

    notes

--- threadPool.c
+++ threadPool.c
@@ -3,20 +3,7 @@
  * 316584960.
  */
 
-#include <malloc.h>
-#include <pthread.h>
 #include "threadPool.h"
-#include<stdio.h>
-#include<string.h>
-#include<pthread.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include <semaphore.h>
-
-//todo: allocations - if not null, handlefailure, free memory
-//declerations
-void* execute(void *arg);
-void executeTasks(void *arg);
 
 
 
@@ -35,7 +22,6 @@ ThreadPool* tpCreate(int numOfThreads) {
         handleFailure();
     }
 
-
     (*threadPool).threads =(pthread_t*) malloc(numOfThreads * sizeof(pthread_t));
     //lock is empty
     pthread_mutex_lock(&threadPool->lockIsEmpty);
@@ -49,16 +35,16 @@ ThreadPool* tpCreate(int numOfThreads) {
     }
     return threadPool;
 }
+
 void* execute(void *arg) {
     ThreadPool *pool = (ThreadPool *)arg;
     (*pool).executeTasks(arg);
 }
+
 void executeTasks(void *arg) {
     ThreadPool *pool = (ThreadPool *)arg;
     while (pool->destroyState==GO||pool->destroyState==BEFORE_JOIN||pool->destroyState==DESTROY1) {
         //if queue is empty - then wait. otherwise - keep running
-        // sem_wait(!osIsQueueEmpty(pool->tasksQueue));
-        //sem_wait();
         pthread_mutex_lock(&(*pool).lockQueue);
         if (pool->destroyState==GO||pool->destroyState==BEFORE_JOIN||pool->destroyState==DESTROY1) {
             if (osIsQueueEmpty(pool->tasksQueue)) {
@@ -76,12 +62,10 @@ void executeTasks(void *arg) {
                 task->function(task->args);
                 free(task);
 
-
             }else {
                 pthread_mutex_unlock(&(*pool).lockQueue);
-                //sleep(1);
-
             }
+            //handle destroy
             if(pool->destroyState==BEFORE_JOIN) {
                 break;
             }
@@ -97,20 +81,8 @@ void tpDestroy(ThreadPool* threadPool, int shouldWaitForTasks) {
     }
     pthread_mutex_unlock(&(*threadPool).lockIsStopped);
 
-
     if (shouldWaitForTasks != 0) {
         threadPool->destroyState=DESTROY1;
-        //if queue is empty - then keep running. otherwise - wait
-       /* while (1) {
-            if (osIsQueueEmpty(threadPool->tasksQueue)) {
-                break;
-            } else {
-                sleep(1);
-            }
-        }*/
-
-
-        //now, the queue is empty
         joinAllThreads(threadPool);
 
     } else if (shouldWaitForTasks == 0) {
@@ -118,10 +90,6 @@ void tpDestroy(ThreadPool* threadPool, int shouldWaitForTasks) {
         threadPool->destroyState=BEFORE_JOIN;
         joinAllThreads(threadPool);
     }
-
-    //pthread_mutex_unlock(&(*threadPool).lockQueue);
-
-
 }
 
 void joinAllThreads(ThreadPool* threadPool) {
@@ -135,25 +103,15 @@ void joinAllThreads(ThreadPool* threadPool) {
         pthread_join(threadPool->threads[i],NULL);
     }
     threadPool->destroyState=AFTER_JOIN;
-
-    //indicats the thread pool is empty
-   // threadPool->tasksQueue->head==NULL;
-    //threadPool->tasksQueue->tail==NULL;
-
-   /* if (pthread_mutex_destroy(&threadPool->lockQueue)||pthread_mutex_destroy(&threadPool->lockIsEmpty)
-        ||pthread_mutex_destroy(&threadPool->lockIsStopped)) {
-        handleFailure();
-    }*/
+    //free of memory allocation
     for (;i<threadPool->size;i++) {
         free(threadPool->threads[i]);
     }
     osDestroyQueue(threadPool->tasksQueue);
     free(threadPool);
 }
-/**
- * handleFailure function.
- * prints error and exits the program.
- */
+
+
 void handleFailure() {
     write(STDERR, ERROR, ERROR_SIZE);
     exit(FAIL);
@@ -178,7 +136,6 @@ int tpInsertTask(ThreadPool* threadPool, void (*computeFunc) (void *), void* par
 
     //add task to queue
     pthread_mutex_lock(&threadPool->lockQueue);
-
     osEnqueue(threadPool->tasksQueue,task);
     pthread_mutex_unlock(&threadPool->lockQueue);
 
--- threadPool.h
+++ threadPool.h
@@ -8,6 +8,13 @@
 
 #include <sys/types.h>
 #include "osqueue.h"
+#include<stdlib.h>
+#include<unistd.h>
+#include <malloc.h>
+#include <pthread.h>
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
 #define TRUE 1
 #define FALSE 0
 #define ERROR "Error in system call\n"
@@ -16,9 +23,9 @@
 #define ERROR_SIZE 21
 #define SUCCESS 0
 
+//enums
 enum DestroyState {BEFORE_JOIN,AFTER_JOIN,GO,DESTROY1};
-//typedef enum destroyState {FIRST, SECOND} DestroyState;
-
+//structs
 typedef struct thread_pool
 {
     int size;
@@ -39,13 +46,44 @@ typedef struct task
     void * args;
 
 }Task;
-
-
-
+/**
+ * tpCreate function.
+ * @param numOfThreads - num of thread's pool threads.
+ * @return the new thread pool.
+ */
 ThreadPool* tpCreate(int numOfThreads);
-
+/**
+ * tpDestroy function.
+ * @param threadPool - the thread pool to be destroyed.
+ * @param shouldWaitForTasks - indicate if we need to wait till all tasks would be completed.
+ */
 void tpDestroy(ThreadPool* threadPool, int shouldWaitForTasks);
-
+/**
+ * tpInsertTask function.
+ * insert new task to the queue.
+ * @param threadPool - the thread pool the task is insert to.
+ * @param computeFunc - the function to be performed.
+ * @param param - args.
+ * @return Fail or success.
+ */
 int tpInsertTask(ThreadPool* threadPool, void (*computeFunc) (void *), void* param);
+/**
+ * execute function.
+ * @param arg - args.
+ * @return void *
+ */
+void* execute(void *arg);
+/**
+ * executeTasks function.
+ * this function handles the thread life cycle.
+ * @param arg - func args.
+ */
+void executeTasks(void *arg);
+
+/**
+ * handleFailure function.
+ * prints error and exits the program.
+ */
+void handleFailure();
 
 #endif
